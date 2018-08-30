b756509a8c4c9cec715f63362941d74d0c33a9b0
LucileMo <lucile.morin@outlook.fr>
2018-08-30 16:07:40 +0200

    accueil

---
+++ sopramon-web/src/main/java/controller/AccueilController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class AccueilController {
+	/*	@GetMapping(value="/Accueil")
+	public String home(@RequestParam(defaultValue="there") String username, Model model) {
+		model.addAttribute("utilisateur", username);
+		return "home";
+	}*/
+
+
+}
