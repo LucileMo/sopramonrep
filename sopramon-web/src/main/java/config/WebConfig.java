97bfc81c741c9f346aaeeae7a1488cfc3933ca7d
LucileMo <lucile.morin@outlook.fr>
2018-08-30 15:35:11 +0200

    config et controller

--- sopramon-web/src/main/java/config/WebConfig.java
+++ sopramon-web/src/main/java/config/WebConfig.java
@@ -1,4 +1,6 @@
+package config;
 import org.springframework.context.annotation.Bean;
+
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
@@ -36,4 +38,4 @@ public class WebConfig {
 
 
 	
-}
+
