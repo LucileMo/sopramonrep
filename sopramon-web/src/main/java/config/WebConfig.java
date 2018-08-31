ff1d56e12bc3c99ca9f3f84b041fac1b02ecc7c8
LucileMo <lucile.morin@outlook.fr>
2018-08-31 10:03:32 +0200

    lksjdkwsj

--- sopramon-web/src/main/java/config/WebConfig.java
+++ sopramon-web/src/main/java/config/WebConfig.java
@@ -1,5 +1,6 @@
 package config;
 import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -25,6 +26,7 @@ public void addResourceHandlers(ResourceHandlerRegistry registry) {
 registry.addResourceHandler("/css/**").addResourceLocations("/css/");
 registry.addResourceHandler("/pictures/**").addResourceLocations("/pictures/");
 }
+
 
 
 	
