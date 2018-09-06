486a8cd45e9a29482ab31385d4070289e78c00cc
Unknown <briac.loyer@gmail.com>
2018-09-06 15:26:01 +0200

    zsczd

--- sopramon-spring-boot/src/main/java/sopramon/controller/CombatController.java
+++ sopramon-spring-boot/src/main/java/sopramon/controller/CombatController.java
@@ -3,6 +3,7 @@ package sopramon.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,28 +16,32 @@ import sopramon.dao.IDAOCoup;
 @Controller
 public class CombatController {
 	
-	
+/*	
 	@GetMapping(value="/Combat")
 	public String Accueil(@RequestParam(defaultValue="there") String username, Model model) {
 		model.addAttribute("utilisateur", username);
 		return "Combat";
 	}
 
-	
+*/	
 	// LISTER LES COMBATS
 	
-//	@Autowired 
-//	private IDAOCombat daoCombat;
-//	
-//	@GetMapping({ "/Combat" })
-//	public String listecombat(Model model) {
-//		 	
-//	model.addAttribute("combats", daoCombat.findAll());
-//	
-//	return "Combat";
-//
-//	
-//	}
+
+ 	
+	@Autowired 
+	private IDAOCombat daoCombat;
+	
+	@Secured({"ROLE_ADMIN, ROLE_USER"})
+	
+	@GetMapping({ "/Combat" })
+	public String listecombat(Model model) {
+		 	
+	model.addAttribute("combats", daoCombat.findAll());
+	
+	return "Combat";
+	
+	
+	}
 
 
 	// HISTORIQUE DES COUPS
