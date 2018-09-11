ab31aca97cd3bd1bdf124a0548d4566a45f4de07
LucileMo <lucile.morin@outlook.fr>
2018-09-11 15:52:18 +0200

    sfhdh

---
+++ sopramon-angular/app/app-config.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+ @Injectable()
+ export class AppConfigService {
+     private apiUrl: string = "http://localhost:8080/api/";
+     private apiUser: string = "admin";
+     private apiPassword: string = "123456";
+
+     public getApiUrl(): string {
+         return this.apiUrl;
+     }
+
+     public getApiBasic(): string {
+         return 'Basic ' + btoa(this.apiUser + ':' + this.apiPassword);
+     }
+ }
