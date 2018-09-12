95329fdca98f569ae67cbfc745d6dee71f919856
Unknown <briac.loyer@gmail.com>
2018-09-12 14:32:33 +0200

    liste

--- sopramon-angular/app/combat.component.ts
+++ sopramon-angular/app/combat.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { Combat } from './combat';
+import {Combat } from './combat';
 import { CombatService } from './combat.service';
-
 
 @Component({
     selector: 'sopra-app',
@@ -9,16 +8,9 @@ import { CombatService } from './combat.service';
     styleUrls: [ 'app/app.component.css' ]
 })
 export class CombatComponent {
-    private combat: Combat = new Combat();
-    private id: number;
-    private date: string;
-    private combats: Array<Combat> = new Array<Combat>();
-//    private arene: Arene ="";
-  //  private type: Type = "";
-
     constructor(private combatService: CombatService) {
-      this.combats.push(new Combat(1,"11092018"))
-          }
+
+    }
 
     public getCombats(): Array<Combat> {
       return this.combatService.findAll();
